b29f250bc7b6ebec5c6ae385aa81b67dde2e053b
Kennedy Mwavu <mwavukennedy@gmail.com>
2024-08-01 05:45:22 +0300

    ch: update authors

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,6 +1,6 @@
 proposal<-"Boilerplate ISC Proposal"
 proposal.file<-"isc-proposal.Rmd"
-author<-"Steph Locke"
+author<-c("John Coene", "Kennedy Mwavu")
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
